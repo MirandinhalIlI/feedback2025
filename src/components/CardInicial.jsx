2fdc7ba8ce60defc5dc174a9ffedf4fef43e377d
aurenisamiranda1 <aurenisamiranda1@gmail.com>
2025-05-06 17:59:51 +0000

    mudanças finais

---
+++ src/components/CardInicial.jsx
@@ -0,0 +1,40 @@
+import iconStar from "../assets/icon-star.svg"
+import { InputButton } from "./InputButton"
+
+
+export function CardInicial({ nota, setNota, setSubimit }) { 
+   
+
+       function handleSubmit() {
+        if(nota !== 0) {
+          setSubimit(true)
+          return 
+        }
+    
+         alert("Please, select a rate button!")
+        }
+
+    return (
+         <div className=" max-w-103 bg-gradient-dark p-6 rounded-2xl  text-white font-overpass">
+        <div className= "bg-dark-blue p-4 w-fit rounded-full mb-4">
+         <img src= {iconStar} alt="icon star" />
+        </div>
+     
+      <h1 className="text-2xl font-bold mb-2.5">How did we do?</h1>
+   
+      <p className="text-sm text-light-grey mb-6">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+   
+       <div className="flex justify-between mb-6">
+         <InputButton valor={1} setNota={setNota} />
+         <InputButton valor={2} setNota={setNota} />
+         <InputButton valor={3} setNota={setNota} />
+         <InputButton valor={4} setNota={setNota} />
+         <InputButton valor={5} setNota={setNota} />
+
+       </div>
+   
+      <button onClick={handleSubmit} className="bg-orange w-full py-3 rounded-full text-sm font-bold uppercase tracking-1 hover:bg-white hover:text-orange cursor-pointer"
+      >Submit</button>
+      </div> 
+    )
+}
